0ae6a9c6291aaa2872374f072ac79fdb8400547c
younesfourti <61004547+younesfourti@users.noreply.github.com>
2023-01-20 15:32:12 +0100

    wordfence + WooCommerce

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1674829322;s:7:"authKey";s:64:"SIq0R7tNMG!!hOMZy)o9t{]{r?K){%F#>JJ_c8+=/FgK I1kW27%^EB4d92Zp($^";s:7:"version";s:5:"1.0.5";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:1103;}
